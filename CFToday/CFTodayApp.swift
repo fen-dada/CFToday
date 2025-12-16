ced1c0f7b845295dab2213e4ce9703bdfaab180d
fen-dada <1823215739@qq.com>
2025-12-16 14:03:49 +0800

    Initial Commit

---
+++ CFToday/CFTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFTodayApp.swift
+//  CFToday
+//
+//  Created by Fendada on 2025/12/16.
+//
+
+import SwiftUI
+
+@main
+struct CFTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CFToday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CFToday
+//
+//  Created by Fendada on 2025/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
